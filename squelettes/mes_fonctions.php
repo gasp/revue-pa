ed2f07aac108297922f23ea1c7f0f29c1936542d
gaspard <gaspard@gmail.com>
2015-10-01 16:27:20 +0200

    shopping pages

--- squelettes/mes_fonctions.php
+++ squelettes/mes_fonctions.php
@@ -1,9 +1,11 @@
 <?php
 
 function filtre_paypal_url($v, $prefix, $price){
+  // $v = preg_replace('/[^\w\s]/', " ", $v);
+  $v = substr($v , 0, 30);
   return 'https://www.paypal.com/cgi-bin/webscr?business='
     .'administration@revue-pa.com'
-    .'&cmd=_xclick&currency_code=EUR'
+    .'&cmd=_cart&currency_code=EUR&add=1'
     .'&amount='.$price
     .'&item_name='.rawurlencode(translitteration($prefix.' '.$v));
 }
