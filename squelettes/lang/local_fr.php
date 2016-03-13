18584fc95d604bc4342bacafb5b67d0cd55f1ef3
gaspard <gaspard@gmail.com>
2016-03-13 21:18:54 +0100

    forum: lang file

---
+++ squelettes/lang/local_fr.php
@@ -0,0 +1,7 @@
+<?php
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'deja_commentaire'=>'commentaire',
+	'deja_commentaires'=>'réactions',
+	'aucun_commentaire'=>'Réagissez à cet article dans notre page de discussion.'
+);
+?>
