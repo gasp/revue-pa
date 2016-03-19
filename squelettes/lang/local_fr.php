34d0883498874fa367ebca44344209b4ee11c934
gaspard <gaspard@gmail.com>
2016-03-19 15:04:00 +0100

    forum: now having a 80px height preview on the article page

--- squelettes/lang/local_fr.php
+++ squelettes/lang/local_fr.php
@@ -1,6 +1,6 @@
 <?php
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	'deja_commentaire'=>'commentaire',
+	'deja_commentaire'=>'un commentaire',
 	'deja_commentaires'=>'réactions',
 	'aucun_commentaire'=>'Réagissez à cet article dans notre page de discussion.'
 );
